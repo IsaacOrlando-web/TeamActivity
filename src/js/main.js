dbcf5b765e040c89393ac85fff9d772003c9a3bd
IsaacOrlando-web <isaacserviciosweb@gmail.com>
2025-07-11 22:48:37 -0400

    Step 5: Working with templates

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,8 @@
 import ProductData from './ProductData.mjs';
+import ProductList from './ProductList.mjs';
 
-const productData = new ProductData('products');
-console.log(productData);
+const productData = new ProductData('tents');
+console.log(productData.getData());
+
+const productList = new ProductList('tents', productData, document.querySelector('.product-list'));
+console.log(productList);
