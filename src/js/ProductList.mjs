5ad37e416e6e4831dda94e5472e301abe23b5756
TonyBRoman <tonybaggon@gmail.com>
2025-07-16 19:21:58 -0600

    Discount indicator

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,6 +1,13 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
+  const isDiscounted = product.FinalPrice < product.SuggestedRetailPrice;
+  const discountPercent = isDiscounted
+    ? Math.round(
+        ((product.SuggestedRetailPrice - product.FinalPrice) / product.SuggestedRetailPrice) * 100
+      )
+    : 0;
+
   return `
     <li class="product-card">
       <a href="/product_pages/?products=${product.Id}">
@@ -8,10 +15,12 @@ function productCardTemplate(product) {
         <h2>${product.Brand.Name}</h2>
         <h3>${product.Name}</h3>
         <p class="product-card__price">$${product.FinalPrice}</p>
+        ${isDiscounted ? `<span class="product-card__discount">-${discountPercent}% OFF</span>`: ""}
       </a>
     </li>
     `;
 }
+
 
 export default class ProductList {
   constructor(category, dataSource, listElement) {
