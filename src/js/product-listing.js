e22b3f1357a5650e398f98cd43768ee4a3ae60bd
IsaacOrlando-web <isaacserviciosweb@gmail.com>
2025-07-18 20:29:11 -0400

    step 5: Pulling the Correct Products

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,15 +1,17 @@
-import {loadHeaderFooter} from "./utils.mjs";
+import {loadHeaderFooter, getParam} from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
 
 loadHeaderFooter();
 
-const dataSource = new ProductData("backpacks");
-console.log(dataSource);
+const category = getParam('category');
+console.log(category);
+
+const dataSource = new ProductData();
 
 const element = document.querySelector(".product-list");
 
-const productList = new ProductList("Tents", dataSource, element);
+const productList = new ProductList(category, dataSource, element);
 
 productList.init();
